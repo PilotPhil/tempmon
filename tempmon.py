9038f7dda3e85986fc480609209659368444aff5
cubic-unit <cadenblox@gmail.com>
2020-10-11 23:23:46 -0600

    Currently functional.
    Former elevater function is in tempmon.py now.
    __name__ == "__main__" pattern implemented.

--- tempmon.py
+++ tempmon.py
@@ -4,38 +4,42 @@ import elevate  # PyPi module for requesting administrator permissions
 import modules.gui as gui  # private module for GUI control
 import modules.tmsettings as tmsettings  # TempMon settings module
 
+def main():
+    # Elevate UAC
+    if ctypes.windll.shell32.IsUserAnAdmin():
+        pass
+    else:
+        print("Not elevated. Attempting UAC elevation.")
+        try:
+            elevate.elevate()
+        except OSError:
+            print("Elevation failed.")
+            print("Exiting with sys code: 1")
+            sys.exit(1)
 
-# Elevate UAC
-if ctypes.windll.shell32.IsUserAnAdmin():
-    pass
-else:
-    print("Not elevated. Attempting UAC elevation.")
-    try:
-        elevate.elevate()
-    except OSError:
-        print("Elevation failed.")
-        print("Exiting with sys code: 1")
-        sys.exit(1)
+
+    # Reading my config here and passing values to my gui as a dictionary
+    # ...but how do I pass it back to the settings writer?
+    # hm...
+    # loaded_settings = tmsettings.settings().import_config()
+    tm_settings_handle = tmsettings.settings()
+
+    # Instatiate a settings observer and attach it to GUI subject
+    # sett_observer = tmsettings.Observer()
+    # g.settings().attach(sett_observer)
+
+    # # Send settings to GUI
+    # g.settings().set_vars(loaded_settings)
+
+    # Initialize gui, make handle, pass settings for init
+    g = gui.gui(tm_settings_handle.import_config(), tm_settings_handle)
+
+    # Define layout
+    g.make_gui()
+
+    # Kick it off.
+    g.start_gui()
 
 
-# Reading my config here and passing values to my gui as a dictionary
-# ...but how do I pass it back to the settings writer?
-# hm...
-# loaded_settings = tmsettings.settings().import_config()
-tm_settings_handle = tmsettings.settings()
-
-# Instatiate a settings observer and attach it to GUI subject
-# sett_observer = tmsettings.Observer()
-# g.settings().attach(sett_observer)
-
-# # Send settings to GUI
-# g.settings().set_vars(loaded_settings)
-
-# Initialize gui, make handle, pass settings for init
-g = gui.gui(tm_settings_handle.import_config(), tm_settings_handle)
-
-# Define layout
-g.make_gui()
-
-# Kick it off.
-g.start_gui()
+if __name__ == "__main__":
+    main()
